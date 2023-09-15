3a2498588e570b8ac9c7247c92b437518b900bd7
NithishNithi <nithishthangarasu02@gmail.com>
2023-09-15 16:59:51 +0530

    1

---
+++ database/database.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/NithishNithi/GoShop/constants"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func ConnectDatabase() (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	mongoConnection := options.Client().ApplyURI(constants.ConnectionString)
+	MongoClient, err := mongo.Connect(ctx, mongoConnection)
+	if err != nil {
+		log.Fatal()
+		return nil, err
+	}
+
+	err1 := MongoClient.Ping(ctx, readpref.Primary())
+	if err1 != nil {
+		return nil, err1
+	}
+	return MongoClient, nil
+}
+
+func GetCollection(client *mongo.Client, dbName string, collectionName string) *mongo.Collection {
+	collection := client.Database(dbName).Collection(collectionName)
+	return collection
+}
